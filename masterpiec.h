b8727288ed44df088d24984cbd769ec89cda99f3
Rafal Gwizdala <rafal@cogit.pl>
2020-02-20 17:12:10 +0100

    save

---
+++ masterpiec.h
@@ -0,0 +1,149 @@
+#ifndef _MASTERPIEC_H_INCLUDED_
+#define _MASTERPIEC_H_INCLUDED_
+
+//zmienne globalne
+
+//temperatura na piecu (woda w piecu)
+double TPiec = 0.0;
+//woda w bojlerze
+double TCwu = 0.0;
+
+
+
+typedef struct BurnParams {
+    uint8_t CycleSec;
+    //czas podawania wegla, * 10 (50 = 5 sekund)
+    uint8_t FuelSecT10;
+    //moc nadmuchu
+    uint8_t BlowerPower;
+    uint8_t BlowerCycle; //cykl dmuchawy dla zasilania grupowego. 0 gdy fazowe.
+} TBurnParams;
+
+//podtrzymanie - czas podawania, czestosc podawania, czas nadmuchu, czestosc nadmuchu, moc nadmuchu
+
+
+typedef uint8_t TSTATE;
+
+#define STATE_UNDEFINED 0
+#define STATE_P0 1   //podtrzymanie
+#define STATE_P1 2   //grzanie z mocą minimalną
+#define STATE_P2 3   //grzanie z mocą nominalną
+#define STATE_STOP 5 //zatrzymany piec (brak nadmuchu, stop pomp, stop podajnika, brak podtrzymania)
+#define STATE_ALARM 6 // alarm - cos się stało, piec zatrzymany albo włączone zabezpieczenie
+
+TSTATE CUR_STATE;  //aktualny stan pieca
+
+#define MAX_POWER_STATES 3 //max liczba konfiguracji dla mocy. 1 - tylko podtrzymanie, 2 - podtrzymanie i praca, 3 - podtrzymanie i 2 moce pracy
+
+void setPumpOn(uint8_t num);
+void setPumpOff(uint8_t num);
+bool isPumpOn(uint8_t num);
+bool isPumpEnabled(uint8_t num);
+
+//uruchomienie podajnika
+void setFeederOn();
+//zatrzymanie podajnika
+void setFeederOff();
+//czy podajnik działa
+bool isFeederOn();
+
+void setBlowerPower(uint8_t power);
+uint8_t getCurrentBlowerPower();
+
+//ostatni odczyt z podanego termometru
+double getLastTempSensorRead(uint8_t sensor);
+//czy termometr jest dostępny
+bool   isTempSensorEnabled(uint8_t sensor);
+
+
+
+
+#define PUMP_CO1 0
+#define PUMP_CWU1 1
+#define PUMP_CO2 2
+#define PUMP_CO3 3
+
+#define TEMP_BOILER 0   //czujnik temp wody
+#define TEMP_CWU 1      //czujnik temp cwu
+#define TEMP_RETURN 2   //czujnik temp powrotu
+#define TEMP_EXHAUST 3  //czujnik spalin
+#define TEMP_EXTERNAL 4 //czujnik temp zewn
+#define TEMP_FUEL 5     //temp podajnika
+#define TEMP_BURNER 6   //temp palnika
+#define TEMP_USER1  7   //dodatkowy czujnik 1
+#define TEMP_USER2  8   //dodatkowy czujnik 2
+
+#define MAX_TEMP_SENSOR 10
+
+
+typedef struct BurnTransition {
+  TSTATE From;
+  TSTATE To;
+  bool (*fCondition)();
+  void (*fAction)(); //akcja wykonywana przy tym przejściu
+} TBurnTransition;
+
+typedef struct BurnStateConfig {
+  TSTATE State;
+  char Code;
+  void (*fLoop)();
+  void (*fInitialize)();
+} TBurnStateConfig;
+
+extern const TBurnTransition  BURN_TRANSITIONS[];
+extern const TBurnStateConfig BURN_STATES[];
+extern TSTATE g_BurnState;
+
+//funkcja wołana cykliczne w celu obsługi 
+//procesu palenia (sterowanie podajnikiem, dmuchawą, przejścia miedzy mocami)
+void burningProc();
+
+//funkcja wołana cyklicznie w celu obsługi zmiany stanu pomp
+void updatePumpStatus();
+
+//funkcja wołana cyklicznie w celu aktualizacji wyswietlacza
+void updateUI();
+
+//cyklicznie, na koncu cyklu - wysylamy prosbe o odczyt wartosci sensorow
+void requestSensorUpdate();
+
+//odczytuje wskazania sensorów i aktualizuje zmienne na tej podstawie
+void readSensorValues();
+
+typedef struct ControlConfiguration {
+  uint8_t TCO;
+  uint8_t TCWU;
+  uint8_t TMinPomp;
+  uint8_t THistCwu; //histereza cwu
+  uint8_t THistCO;  //histereza co
+  uint8_t TDeltaCO; //delta co - temp powyzej zadanej przy ktorej przejdzie w podtrzymanie
+  uint8_t TDeltaCWU; //delta cwu - temp powyżej bojlera do ktorej rozgrzewamy piec
+  uint16_t T10PodtrzymaniePrzedmuch; //czas cyklu przedmuchu w podtrzymaniu, * 10 (100 = 10 sek)
+  uint16_t T10PodtrzymanieDmuch; //czas pracy dmuchawy w podtrzymaniu
+  uint8_t P0MocDm; //P0 moc dmuchawy w podtrzymaniu
+  
+  TBurnParams BurnConfigs[MAX_POWER_STATES];
+  
+} TControlConfiguration;
+
+extern TControlConfiguration g_CurrentConfig;
+//restore saved configuration on program init
+//from a specified slot. 0 is the default slot
+void eepromRestoreConfig(uint8_t configSlot);
+
+//store current configuration in specified config slot
+void eepromSaveConfig(uint8_t configSlot);
+
+//reset config to default
+void eepromResetConfig(uint8_t configSlot);
+
+
+//zdarzenia ui
+#define UI_EV_DOWN 2   //w lewo (dodatkowy parametr = increment)
+#define UI_EV_UP 3     //w prawo (dodatkowy parametry = increment)
+#define UI_EV_BTNPRESS 4 //guzik. dodatkowy parametr = który
+#define UI_EV_BTNRELEASE 5   //guzik, zwolnienie przycisku
+
+void processUIEvent(uint8_t event, int8_t arg);
+
+#endif
---
+++ masterpiec.ino
@@ -0,0 +1,154 @@
+#include "masterpiec.h"
+#include <assert.h>
+
+/**
+ * struktura programu
+ * - sterowanie silnikiem dmuchawy - na przerwaniu 0
+ * - pętla sterowania grzaniem (tj załączanie dmuchawy i podajnika w odpowiednim reżimie) - na przerwaniu zegarowym, aktualizacja statusu co 1 sek. Pętla sterowania grzaniem obsługuje też wyłączanie i załączanie pomp.
+ * - obsługa UI. Aktualizacja ekranu w głównym loop-ie, natomiast obsługa inputu - z przerwań. Nie powinno być wyswietlania czegokolwiek w przerwaniu.
+ * - zewnętrzny termostat - podpięty do jednego z pinów. Odczytywany tryb (grzej / nie grzej) i obsługiwany odpowiednio w pętli sterowania grzaniem
+ * - odczyt czujników temperatury - też powinien następować w pętli, nie wiem czy tak często jak pętla sterowania grzaniem
+ * 
+ * 
+ * pompy - działają niezależnie. Jeśli grzanie=ON to pompa CO załącza się gdy temp. jest powyżej minimalnej i wyłącza się gdy jest poniżej. Jeśli grzanie = off to pompa CO załącza się wyłącznie po to żeby pozbyć się nadmiaru ciepła, czyli gdy temp aktualna CO jest wyższa 
+ * niż TZadana + delta. Pozbywanie się nadmiaru ciepła trwa do momentu gdy temp spadnie albo gdy przestanie rosnąć albo przez ustalony cykl. Możemy też pompę co skonfigurować do załączania się cyklicznie na x minut co y minut.
+ * Pompa CWU - działa gdy temp pieca jest wyższa niż temp bojlera+delta CWU (czyli to jest nasza minimalna temp załączenia pompy). Załącza się gdy temp bojlera spadnie poniżej tCWUZadana - hist1. W momencie załączenia przestawiamy TZadana pieca na tZadanaCWU+delta CWU
+ * pompa CWU wyłącza się gdy temp bojlera osiągnie docelową (wtedy tez przywracamy temp zadana pieca) albo gdy temp spadnie poniżej tBojler+delta cwu.
+ * 
+ */
+void setup() {
+  //initialize interrupts etc
+  //initialize hardware
+  
+  // put your setup code here, to run once:
+  eepromRestoreConfig(0);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+
+TSTATE g_BurnState = STATE_UNDEFINED;
+//czas wejscia w bieżący stan, ms
+unsigned long g_CurStateStart = 0;
+uint16_t  g_CurStateStartC10 = 0; //temp pieca w momencie wejscia w bież. stan.
+unsigned long g_CurBurnCycleStart = 0; //timestamp, w ms, w ktorym rozpoczelismy akt. cykl palenia
+uint16_t g_BoilerC10; //aktualna temperatura pieca
+
+TControlConfiguration g_CurrentConfig;
+
+//to nasza procedura aktualizacji stanu hardware-u
+//wolana cyklicznie.
+void standardBurnLoop() {
+  readSensorValues();
+  burningProc();
+  updatePumpStatus();
+  requestSensorUpdate();
+}
+
+void burningProc() 
+{
+  assert(g_BurnState != STATE_UNDEFINED);
+  //1. check if we should change state
+  uint8_t i=0;
+  while(BURN_TRANSITIONS[i].From != STATE_UNDEFINED)
+  {
+    if (BURN_TRANSITIONS[i].From == g_BurnState) 
+    {
+      if (BURN_TRANSITIONS[i].fCondition()) 
+      {
+        //transition to new state
+        g_BurnState = BURN_TRANSITIONS[i].To;
+        assert(g_BurnState != STATE_UNDEFINED);
+        g_CurStateStart = millis();
+        g_CurStateStartC10 = g_BoilerC10;
+        BURN_STATES[g_BurnState].fInitialize();
+        return;    
+      }
+    }
+  }
+  
+  //2. obsługa bieżącego stanu
+  BURN_STATES[g_BurnState].fLoop();
+  
+  
+}
+
+
+
+static unsigned long burnCycleLen = 0;
+static unsigned long burnFeedLen = 0;
+static unsigned long burnCycleStart = 0;
+
+void workStateInitialize() {
+  assert(g_BurnState != STATE_UNDEFINED && g_BurnState != STATE_STOP);
+  burnCycleLen = g_CurrentConfig.BurnConfigs[g_BurnState].CycleSec * 1000;
+  burnFeedLen = g_CurrentConfig.BurnConfigs[g_BurnState].FuelSecT10 * 100;
+  burnCycleStart = g_CurStateStart;
+  setBlowerPower(g_CurrentConfig.BurnConfigs[g_BurnState].BlowerPower);
+}
+
+///pętla palenia dla stanu pracy
+//załączamy dmuchawę na ustaloną moc no i pilnujemy podajnika
+void workStateBurnLoop() {
+  assert(g_BurnState != STATE_UNDEFINED && g_BurnState != STATE_STOP);
+  unsigned long tNow = millis();
+  if (tNow < burnCycleStart + burnFeedLen) 
+  {
+    if (!isFeederOn()) setFeederOn();
+  }
+  else 
+  {
+    if (isFeederOn()) setFeederOff();
+  }
+  if (tNow >= burnCycleStart + burnCycleLen) {
+    burnCycleStart = millis();
+  }
+}
+
+void podtrzymanieStateInitialize() {
+  
+}
+
+void podtrzymanieStateLoop() {
+  unsigned long tNow = millis();
+  
+}
+
+void eepromRestoreConfig(uint8_t slot) {
+  
+}
+
+
+
+
+
+const TBurnTransition  BURN_TRANSITIONS[]  = 
+{
+  {STATE_STOP, STATE_P2, NULL, NULL},  
+  {STATE_P2, STATE_P1, NULL, NULL},
+  {STATE_P1, STATE_P2, NULL, NULL},
+  {STATE_P1, STATE_P0, NULL, NULL},
+  {STATE_P0, STATE_P2, NULL, NULL},
+  {STATE_P2, STATE_P1, NULL, NULL},
+  {STATE_P0, STATE_ALARM, NULL, NULL},
+  {STATE_P1, STATE_ALARM, NULL, NULL},
+  {STATE_P2, STATE_ALARM, NULL, NULL},
+  {STATE_STOP, STATE_ALARM, NULL, NULL},
+
+  
+  {STATE_UNDEFINED, STATE_UNDEFINED, NULL, NULL} //sentinel
+};
+
+const TBurnStateConfig BURN_STATES[] = {
+  {STATE_UNDEFINED, '~', NULL, NULL},
+  {STATE_P0, 'P', podtrzymanieStateInitialize, podtrzymanieStateLoop},
+  {STATE_P1, '1', workStateInitialize, workStateBurnLoop},
+  {STATE_P2, '2', workStateInitialize, workStateBurnLoop},
+  {STATE_STOP, 'S', NULL, NULL},
+  {STATE_ALARM, 'A', NULL, NULL},
+  {STATE_UNDEFINED, ' ', NULL, NULL} //sentinel
+};
